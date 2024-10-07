ab5425faf6f5ff23b0ecd488e390ca1b2036cc64
valorose <olav.rosness@gmail.com>
2024-10-07 11:07:48 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,16 +1,19 @@
-let ratingsData;
+let ratingsData = {};
 
 fetch('ratings.json')
   .then(response => response.json())
   .then(data => {
-    ratingsData = data;
-    chrome.storage.local.set({ratingsData: data});
+    // Process the data into a more easily searchable format
+    data.forEach(item => {
+      ratingsData[item.navn.toLowerCase()] = item.total_karakter;
+    });
+    chrome.storage.local.set({ratingsData: ratingsData});
   })
   .catch(error => console.error('Error loading ratings data:', error));
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'getRating') {
-    const rating = ratingsData[request.domain];
+    const rating = ratingsData[request.name.toLowerCase()];
     sendResponse({rating: rating});
   }
   return true;  // Indicates that the response is sent asynchronously
